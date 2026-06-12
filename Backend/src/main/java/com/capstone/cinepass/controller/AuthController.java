8b4d68faf83dba89bda84a21d3875779fceac67e
Sabbir <sabbir.mathakiya@apexon.com>
2026-06-12 15:34:28 +0530

    Updated pom, added dependencies and added few classes

--- Backend/src/main/java/com/capstone/cinepass/controller/AuthController.java
+++ Backend/src/main/java/com/capstone/cinepass/controller/AuthController.java
@@ -1,9 +1,17 @@
 package com.capstone.cinepass.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
+    @PostMapping("/register")
+    public void register() {
 
+    }
 }
